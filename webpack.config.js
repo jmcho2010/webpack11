a7b4050a41bb5275203cea35691e8c9ebb34bc02
jmcho2010 <projm2786@gmail.com>
2025-02-19 11:10:47 +0900

    ㅅㄷㄴㅅ

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    entry: {
+        main: './src/main.js',
+        sub: './src/sub.js',
+        gunchim: './src/gunchim.js'    
+    },
+    output: {
+        //기본값으로 설정되는 옵션이 있긴함
+        //dist : distribution의 약자
+        path: path.resolve(__dirname, 'dist'),
+    }, // 출력
+    module: {
+        rules: [
+          {
+            test: /\.s?css$/,
+            use: [
+              // 순서 중요!
+              'style-loader',
+              'css-loader',
+              'sass-loader'
+            ]
+          },
+          {
+            test: /\.jsx?$/,
+            use: 'swc-loader'
+          },
+          {
+            test: /\.(png|jpe?g|svg|gif|webp)$/i, // i 옵션 - 대소문자 구분 없음
+            type: 'asset/resource' // type을 통해 Webpack에 내장된 로더(Builtin Loader) 명시
+          }
+        ]
+      },
+    plugins: [], // 플러그인
+    // 기타 옵션...
+  }
